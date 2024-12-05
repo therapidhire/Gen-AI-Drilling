c88068794cb01fce1e742dd12ee293381a59dcff
Vinit Pawar <rapidhirecs10@gmail.com>
2024-12-05 19:13:55 +0530

    first commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World! vinit");
 });
 
 // app.use("/contact-us", WhatsAppConnectRoute);
