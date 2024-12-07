c2afe926b651643fcb60e6b5c0148ad5c4d2a89f
Vinit Pawar <rapidhirecs10@gmail.com>
2024-12-07 19:17:19 +0530

    created getStockDetailsByUserId api.

--- src/web/controllers/stock/getStockDetails/index.js
+++ src/web/controllers/stock/getStockDetails/index.js
@@ -2,43 +2,6 @@ const StockDetailTable = require("../../../../models/stockDetailTable");
 const StockList = require("../../../../models/masterStockList");
 
 // Get all stock details (Admin access)
-// const getAllStockDetails = async (req, res) => {
-//   try {
-//     const stockDetails = await StockDetailTable.find();
-//     const stockLists = await StockList.find();
-//     res.status(200).json({
-//       success: true,
-//       data: { stockDetails: stockDetails, stockLists: stockLists },
-//     });
-//   } catch (error) {
-//     res.status(500).json({ success: false, message: error.message });
-//   }
-// };
-// const getAllStockDetails = async (req, res) => {
-//   try {
-//     const mergedData = await StockList.aggregate([
-//       {
-//         $lookup: {
-//           from: "StockDetailTable", // The collection name in the database (should match the collection name in MongoDB)
-//           localField: "stock_Id", // The field in StockDetailTable that relates to StockList
-//           foreignField: "isin_Num", // The field in StockList that matches stockListId
-//           as: "stockListData", // The name of the new array field in the output
-//         },
-//       },
-//       //   {
-//       //     $unwind: "$stockListData", // If you expect only one matching document, this flattens the array
-//       //   },
-//     ]);
-
-//     res.status(200).json({
-//       success: true,
-//       data: mergedData,
-//     });
-//   } catch (error) {
-//     res.status(500).json({ success: false, message: error.message });
-//   }
-// };
-
 const getAllStockDetails = async (req, res) => {
   try {
     const stockDetails = await StockDetailTable.find().populate({
@@ -57,36 +20,48 @@ const getAllStockDetails = async (req, res) => {
 
 const getStockDetailsByUserId = async (req, res) => {
   try {
-    console.log("req.params;", req.params?._id);
+    console.log("Request received with params:", req.params);
 
-    const { _id } = req.params;
+    // Extracting the `_id` parameter from the query
+    const { _id } = req.query;
+    console.log("_id_id_id_id_id_id:", _id);
+
+    // Validate the `_id` parameter
     if (!_id) {
       return res
         .status(400)
-        .json({ success: false, message: "User ID is required." });
+        .json({ success: false, message: "Stock ID is required." });
     }
 
-    const stocks = await StockDetailTable.aggregate([
-      {
-        $lookup: {
-          from: "StockList",
-          localField: "stock_Id",
-          foreignField: "isin_Num",
-          as: "stockInfo",
-        },
-      },
-      {
-        $project: {
-          stockName: { $arrayElemAt: ["$stockInfo.stockName", 0] },
-          symbol: { $arrayElemAt: ["$stockInfo.symbol", 0] },
-          price: 1,
-        },
-      },
-    ]);
+    // Fetch data from StockDetailTable and populate data from StockList
+    const stockDetails = await StockDetailTable.findOne({ _id })
+      .populate({
+        path: "stockListData", // Virtual reference in StockDetailTable schema
+        select: "stockName symbol isin_Num", // Fields to fetch from StockList
+      })
+      .exec();
 
-    res.status(200).json({ success: true, data: stocks });
+    // Handle case when no matching stock is found
+    if (!stockDetails) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Stock not found." });
+    }
+
+    // Send success response with data
+    res.status(200).json({
+      success: true,
+      data: stockDetails,
+    });
   } catch (error) {
-    res.status(500).json({ success: false, message: error.message });
+    console.error("Error occurred in getStockDetailsByUserId:", error);
+
+    // Handle unexpected errors
+    res.status(500).json({
+      success: false,
+      message: "An unexpected error occurred. Please try again later.",
+      error: error.message, // Include error message for debugging
+    });
   }
 };
 
